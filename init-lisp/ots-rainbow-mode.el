fbcf47b86545ee528556ff580c96cca22cd6a475
Osmo Salomaa <otsaloma@iki.fi>
2015-04-08 21:01:36 +0300

    Reverse list of rainbow-mode modes.

--- init-lisp/ots-rainbow-mode.el
+++ init-lisp/ots-rainbow-mode.el
@@ -3,13 +3,14 @@
 
 (require 'rainbow-mode)
 
-(defvar ots-rainbow-mode-no-modes
-  '(change-log-mode text-mode woman-mode)
-  "Modes in which to not color hexadecimal color codes.")
+(defvar ots-rainbow-mode-modes
+  '(conf-mode css-mode emacs-lisp-mode ess-mode inferior-ess-mode js-mode
+    latex-mode python-mode qml-mode)
+  "Modes in which to color hexadecimal color codes.")
 
 (defun ots-rainbow-mode-enable ()
   "Enable rainbow-mode to color color strings."
-  (if (not (member major-mode ots-rainbow-mode-no-modes))
+  (if (member major-mode ots-rainbow-mode-modes)
       (rainbow-mode)))
 
 (add-hook 'after-change-major-mode-hook 'ots-rainbow-mode-enable)
