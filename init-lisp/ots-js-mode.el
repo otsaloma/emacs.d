49db26c8f9cf6ab12fa92be736442f11b9f45c59
Osmo Salomaa <osmo.salomaa@strafica.fi>
2015-05-19 16:36:13 +0300

    js-mode: highlight self and that.

--- init-lisp/ots-js-mode.el
+++ init-lisp/ots-js-mode.el
@@ -4,6 +4,15 @@
 (let ((modes '(("\\.\\(geo\\|topo\\)?json$" . js-mode))))
   (setq auto-mode-alist (append modes auto-mode-alist)))
 
+(defun ots-js-mode-set-faces ()
+  "Set faces for editing JavaScript files."
+  (font-lock-add-keywords
+   nil
+   '(;; Constants
+     ("\\<\\(self\\|that\\)\\>"
+      1 font-lock-constant-face)
+     )))
+
 (defun ots-js-mode-set-properties ()
   "Set properties for editing JavaScript files."
   (local-set-key (kbd "<backspace>") 'backward-delete-char-untabify)
@@ -17,6 +26,7 @@
   (turn-on-auto-fill)
   (setq-local helm-dash-docsets '("JavaScript")))
 
+(add-hook 'js-mode-hook 'ots-js-mode-set-faces)
 (add-hook 'js-mode-hook 'ots-js-mode-set-properties)
 (add-to-list 'interpreter-mode-alist '("node" . js-mode))
 
