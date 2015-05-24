17cd4e226b476c6dcee709f5d573195a6a9a9b02
Osmo Salomaa <otsaloma@iki.fi>
2015-05-24 15:33:28 +0300

    Don't set obsolete default-major-mode.

--- init-lisp/ots-editor.el
+++ init-lisp/ots-editor.el
@@ -8,7 +8,6 @@
 (setq auto-window-vscroll nil)
 (setq backward-delete-char-untabify-method 'hungry)
 (setq compilation-scroll-output t)
-(setq default-major-mode 'text-mode)
 (setq font-lock-maximum-decoration t)
 (setq mouse-drag-copy-region nil)
 (setq require-final-newline t)
@@ -17,11 +16,13 @@
 (setq sort-fold-case t)
 (setq x-select-enable-clipboard t)
 (setq-default fill-column 80)
+(setq-default major-mode 'text-mode)
 (show-paren-mode t)
 (transient-mark-mode t)
 
 ;; Deactivate selection when switching buffers.
 ;; XXX: This is fucking suboptimal...
+(require 'ots-tabbar)
 (advice-add 'switch-to-buffer :before #'deactivate-mark)
 (advice-add 'tabbar-forward-tab :before #'deactivate-mark)
 (advice-add 'tabbar-backward-tab :before #'deactivate-mark)
