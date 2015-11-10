fc5fc151a4ba48cab04c6b45b04247b7a9fd4932
Osmo Salomaa <otsaloma@iki.fi>
2015-11-11 00:39:42 +0200

    Clean up wcheck-mode config.
    Add action to add words to user's dictionary.

--- init-lisp/ots-wcheck-mode.el
+++ init-lisp/ots-wcheck-mode.el
@@ -1,54 +1,58 @@
 ;;; -*- coding: utf-8-unix -*-
 ;;; ots-wcheck-mode.el
 
-(autoload 'wcheck-mode "wcheck-mode" "Toggle wcheck-mode." t)
-(autoload 'wcheck-change-language "wcheck-mode" "Switch language." t)
 (autoload 'wcheck-actions "wcheck-mode" "Open actions menu." t)
+(autoload 'wcheck-change-language "wcheck-mode" "Switch language." t)
+(autoload 'wcheck-mode "wcheck-mode" "Toggle wcheck-mode." t)
+
+(defvar enchant-program
+  (if (eq system-type 'windows-nt)
+      "c:\\cygwin\\bin\\enchant.exe"
+    "/usr/bin/enchant")
+  "Path to the enchant executable.")
+
+(defun enchant-add-to-dictionary (marked-text)
+  "Add word from marked-text to user's dictionary."
+  (let* ((word (aref marked-text 0))
+         (lang (aref marked-text 4))
+         (file (format "~/.config/enchant/%s.dic" lang)))
+    (when (and file (file-writable-p file))
+      (write-region (concat "\n" word) nil file 'append)
+      (message "Added word \"%s\" to the %s.dic" word lang))))
+
+(defun enchant-suggestions-menu (marked-text)
+  "Add to suggestions menu an action to add word to user's dictionary."
+  (cons (cons "[Add]" 'enchant-add-to-dictionary)
+        (wcheck-parser-ispell-suggestions)))
+
+(defun enchant-wcheck-entry (lang)
+  "Construct a wcheck-language-data alist entry for enchant."
+  (setq out (list lang))
+  (add-to-list 'out `(program . ,enchant-program) t)
+  (add-to-list 'out `(args "-l" "-d" ,lang) t)
+  (add-to-list 'out `(action-program . ,enchant-program) t)
+  (add-to-list 'out `(action-args "-a" "-d" ,lang) t)
+  (add-to-list 'out `(action-parser . enchant-suggestions-menu) t)
+  out)
 
 (setq wcheck-language "fi")
-(setq
- wcheck-language-data
- (if (eq system-type 'windows-nt)
-     '(("en"
-        (program . "c:\\cygwin\\bin\\enchant.exe")
-        (args "-l" "-d" "en")
-        (action-program . "c:\\cygwin\\bin\\enchant.exe")
-        (action-args "-a" "-d" "en")
-        (action-parser . wcheck-parser-ispell-suggestions))
-       ("fi"
-        (program . "c:\\cygwin\\bin\\enchant.exe")
-        (args "-l" "-d" "fi")
-        (action-program . "c:\\cygwin\\bin\\enchant.exe")
-        (action-args "-a" "-d" "fi")
-        (action-parser . wcheck-parser-ispell-suggestions)))
-   '(("en"
-      (program . "/usr/bin/enchant")
-      (args "-l" "-d" "en")
-      (action-program . "/usr/bin/enchant")
-      (action-args "-a" "-d" "en")
-      (action-parser . wcheck-parser-ispell-suggestions))
-     ("fi"
-      (program . "/usr/bin/enchant")
-      (args "-l" "-d" "fi")
-      (action-program . "/usr/bin/enchant")
-      (action-args "-a" "-d" "fi")
-      (action-parser . wcheck-parser-ispell-suggestions)))))
-
+(add-to-list 'wcheck-language-data (enchant-wcheck-entry "en") t)
+(add-to-list 'wcheck-language-data (enchant-wcheck-entry "fi") t)
 (setq wcheck-language-data-defaults
       '((read-or-skip-faces
-         (latex-mode
-          read
-          nil
-          font-latex-bold-face
-          font-latex-italic-face
-          font-latex-sectioning-0-face
-          font-latex-sectioning-1-face
-          font-latex-sectioning-2-face
-          font-latex-sectioning-3-face
-          font-latex-sectioning-4-face
-          font-latex-sectioning-5-face
-          font-latex-slide-title-face
-          font-lock-type-face))))
+         (latex-mode read
+                     nil ; normal text
+                     font-latex-bold-face
+                     font-latex-italic-face
+                     font-latex-sectioning-0-face
+                     font-latex-sectioning-1-face
+                     font-latex-sectioning-2-face
+                     font-latex-sectioning-3-face
+                     font-latex-sectioning-4-face
+                     font-latex-sectioning-5-face
+                     font-latex-slide-title-face
+                     font-lock-type-face
+                     ))))
 
 (global-set-key (kbd "<f7>") 'wcheck-mode)
 (global-set-key (kbd "<S-f7>") 'wcheck-change-language)
