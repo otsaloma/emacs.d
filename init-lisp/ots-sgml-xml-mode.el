257903620cb3588b95b9e7fd5b37b8e2aca563d3
Osmo Salomaa <otsaloma@iki.fi>
2016-01-05 12:54:45 +0200

    Open SVG files in xml-mode

--- init-lisp/ots-sgml-xml-mode.el
+++ init-lisp/ots-sgml-xml-mode.el
@@ -1,6 +1,8 @@
 ;;; -*- coding: utf-8 -*-
 ;;; ots-sgml-xml-mode.el
 
+(add-to-list 'auto-mode-alist '("\\.svg" . xml-mode))
+
 (defun ots-sgml-xml-mode-set-properties ()
   "Set properties for editing XML files."
   (local-set-key (kbd "<backspace>") 'backward-delete-char-untabify)
