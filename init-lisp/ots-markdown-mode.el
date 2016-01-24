5987d0375c89b3e8c50693ed0c0abc9c510b3889
Osmo Salomaa <otsaloma@iki.fi>
2016-01-24 02:49:37 +0200

    Use markdown-css
    https://github.com/otsaloma/markdown-css

--- init-lisp/ots-markdown-mode.el
+++ init-lisp/ots-markdown-mode.el
@@ -6,7 +6,7 @@
 (add-to-list 'auto-mode-alist '("\\.md" . markdown-mode))
 
 (defvar ots-markdown-mode-css-file
-  (expand-file-name "~/.local/share/markdown/tufte.css")
+  (expand-file-name "~/.local/share/markdown-css/tufte.css")
   "Name of stylesheet file to use.")
 
 (defvar ots-markdown-mode-command
