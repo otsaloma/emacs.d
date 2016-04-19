caa94113d60487d2f4850d333e5cf43d61a2c50d
Osmo Salomaa <osmo.salomaa@strafica.fi>
2016-04-19 17:43:21 +0300

    Keep neotree window width at its set value

--- init-lisp/ots-neotree.el
+++ init-lisp/ots-neotree.el
@@ -1,11 +1,23 @@
 ;;; -*- coding: utf-8 -*-
 ;;; ots-neotree.el
 
+(require 'neotree)
+
+(defun ots-neotree-ensure-width ()
+  "Reset neotree width to neo-window-width."
+  ;; XXX: Work around a bug in neotree that sometimes
+  ;; shrinks the window upon opening a file.
+  (when (neo-global--window-exists-p)
+    (neo-global--reset-width)
+    (neo-global--reset-width)
+    (neo-global--reset-width)))
+
 (defun ots-neotree-update-width ()
   "Update neotree width to be relative to total frame width."
   (let ((width (window-total-width (frame-root-window))))
     (setq neo-window-width (floor (* 0.3 width)))))
 
+(add-hook 'after-change-major-mode-hook 'ots-neotree-ensure-width t)
 (add-hook 'window-configuration-change-hook 'ots-neotree-update-width t)
 (global-set-key (kbd "<S-f4>") 'neotree-toggle)
 (global-set-key (kbd "<C-S-f4>") 'neotree-find)
