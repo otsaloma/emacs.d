c8ecf30e88ff963b26c4983be51ff866c21bfc26
Osmo Salomaa <otsaloma@iki.fi>
2016-05-07 19:41:52 +0300

    Use conf-mode for .theme files

--- init-lisp/ots-conf-mode.el
+++ init-lisp/ots-conf-mode.el
@@ -1,6 +1,9 @@
 ;;; -*- coding: utf-8 -*-
 ;;; ots-conf-mode.el
 
+(let ((modes '(("\\.theme$" . conf-mode))))
+  (setq auto-mode-alist (append modes auto-mode-alist)))
+
 (defun ots-conf-mode-set-properties ()
   "Set properties for editing configuration files."
   (conf-quote-normal nil)
