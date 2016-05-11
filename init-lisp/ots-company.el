ec47524774aa7c27f93f649a8bc4718140e8a71e
Osmo Salomaa <osmo.salomaa@strafica.fi>
2016-05-11 18:04:17 +0300

    Allow saving while company completions open

--- init-lisp/ots-company.el
+++ init-lisp/ots-company.el
@@ -9,10 +9,17 @@
   (company-abort)
   (execute-kbd-macro (kbd "<return>")))
 
+(defun ots-company-save ()
+  "Just do what the fuck Ctrl+S should do."
+  (interactive)
+  (company-abort)
+  (execute-kbd-macro (kbd "C-s")))
+
 (defun ots-company-set-properties ()
   "Set properties for company-mode buffers."
   (define-key company-active-map (kbd "<tab>") 'company-complete-selection)
   (define-key company-active-map (kbd "<return>") 'ots-company-enter)
+  (define-key company-active-map (kbd "C-s") 'ots-company-save)
   (setq company-idle-delay 0.1)
   (setq company-tooltip-align-annotations t))
 
