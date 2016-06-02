6fd1713c6d4731a21f80ad33339432258b9bb349
Osmo Salomaa <osmo.salomaa@strafica.fi>
2016-06-02 16:32:34 +0300

    Make projectile work on Windows too
    projectile-globally-ignored-file-suffixes don't work though.

--- init-lisp/ots-projectile.el
+++ init-lisp/ots-projectile.el
@@ -7,11 +7,14 @@
 (add-to-list 'projectile-globally-ignored-directories "__pycache__")
 (add-to-list 'projectile-globally-ignored-directories "build")
 (add-to-list 'projectile-globally-ignored-directories "dist")
+(add-to-list 'projectile-globally-ignored-directories "docsets")
 (add-to-list 'projectile-globally-ignored-directories "elpa")
-(add-to-list 'projectile-globally-ignored-directories "node-modules")
+(add-to-list 'projectile-globally-ignored-directories "node_modules")
 
 (add-to-list 'projectile-globally-ignored-file-suffixes ".elc")
 (add-to-list 'projectile-globally-ignored-file-suffixes ".pyc")
+
+(projectile-global-mode)
 
 (provide 'ots-projectile)
 ;;; ots-projectile.el ends here
