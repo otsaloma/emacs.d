a017df3ee514b02ee997d49b8c6a3efb65b3eee1
Osmo Salomaa <osmo.salomaa@strafica.fi>
2016-09-09 14:43:29 +0300

    Add emacs1.bat

--- extra/emacs.bat
+++ extra/emacs.bat
@@ -1,10 +1,8 @@
 @echo off
 if exist "%HOME%\.emacs.d\server\server" (
-    :: Open files given as arguments in already running server.
-    "%PROGRAMFILES(X86)%\GNU Emacs\bin\emacsclientw.exe" ^
-        -n %1 %2 %3 %4 %5 %6 %7 %8 %9
+    :: Open files given as arguments in existing Emacs.
+    "%PROGRAMFILES(X86)%\GNU Emacs\bin\emacsclientw.exe" -n %*
 ) else (
-    :: Start a new emacs, open files given as arguments and start the server.
-    "%PROGRAMFILES(X86)%\GNU Emacs\bin\runemacs.exe" ^
-        -g 90x35 --eval "(server-start)" %1 %2 %3 %4 %5 %6 %7 %8 %9
+    :: Start a new Emacs and open files given as arguments.
+    "%PROGRAMFILES(X86)%\GNU Emacs\bin\runemacs.exe" -g 90x35 --eval "(server-start)" %*
 )
---
+++ extra/emacs1.bat
@@ -0,0 +1,2 @@
+@echo off
+"%PROGRAMFILES(X86)%\GNU Emacs\bin\emacs.exe" -g 90x35 %*
