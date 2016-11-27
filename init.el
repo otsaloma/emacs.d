a9fc86a9ef94861371030478319a3f8b527f7084
Osmo Salomaa <otsaloma@iki.fi>
2016-11-27 21:43:16 +0200

    Remove accidentally added custom shit

--- init.el
+++ init.el
@@ -92,17 +92,3 @@
   (require 'ots-yasnippet))
 
 (run-with-timer 0.001 nil #'(lambda () (message "")))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (graphql-mode wcheck-mode tabbar smooth-scrolling rainbow-mode php-mode pcre2el neotree markdown-mode magit linear-undo js2-mode js-comint helm-projectile helm-dash git-gutter-fringe+ dos csv-mode company-tern company-dict company-auctex company-anaconda))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
