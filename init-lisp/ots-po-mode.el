c9d21c1e5c0fef333be287a5a82cc9b281bca6e1
Osmo Salomaa <otsaloma@iki.fi>
2016-12-28 17:36:33 +0200

    Add spell-checking to the po-mode edit buffer

--- init-lisp/ots-po-mode.el
+++ init-lisp/ots-po-mode.el
@@ -1,6 +1,17 @@
 ;;; -*- coding: utf-8 -*-
 ;;; ots-po-mode.el
 
+(defun ots-po-mode-guess-language ()
+  "Return the language of the current PO file."
+  (save-excursion
+    (goto-char (point-min))
+    (re-search-forward po-any-msgstr-block-regexp)
+    (goto-char (match-beginning 0))
+    (if (re-search-forward
+         "\n\"Language: +\\(.+\\)\\\\n\"$"
+         (match-end 0) t)
+        (po-match-string 1))))
+
 (defun ots-po-mode-set-keys ()
   "Set keybindings for editing PO files."
   (local-set-key (kbd "C-s") 'ots-util-save-buffer)
@@ -12,6 +23,14 @@
   (define-key po-subedit-mode-map (kbd "C-g") 'po-subedit-abort)
   (set (make-local-variable 'po-subedit-message)
        "Type 'C-s' once done, or 'C-g' to abort edit"))
+
+(defadvice po-edit-string (around setup-spell-checking (string type expand-tabs) activate)
+  "Set up spell-checking for the subedit buffer."
+  (let ((lang (ots-po-mode-guess-language)))
+    ad-do-it
+    (when lang
+      (wcheck-change-language lang)
+      (wcheck-mode))))
 
 (add-hook 'po-mode-hook 'ots-po-mode-set-keys)
 (add-hook 'po-subedit-mode-hook 'ots-po-subedit-mode-set-keys)
