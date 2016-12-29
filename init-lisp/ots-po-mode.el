c2cdc0ad0595e8769ecbf4de7d5d796453bb671e
Osmo Salomaa <otsaloma@iki.fi>
2016-12-29 18:11:49 +0200

    Simplify po-mode config

--- init-lisp/ots-po-mode.el
+++ init-lisp/ots-po-mode.el
@@ -12,22 +12,20 @@
          (match-end 0) t)
         (po-match-string 1))))
 
-(defun ots-po-mode-set-keys ()
-  "Set keybindings for editing PO files."
+(defun ots-po-mode-set-properties ()
+  "Set properties for editing PO files."
+  (linum-mode 1)
   (local-set-key (kbd "C-s") 'ots-util-save-buffer)
   (local-set-key (kbd "C-z") 'po-undo))
-
-(defun ots-po-subedit-mode-set-keys ()
-  "Set keybindings for editing PO file entries."
-  (define-key po-subedit-mode-map (kbd "C-s") 'po-subedit-exit)
-  (define-key po-subedit-mode-map (kbd "C-g") 'po-subedit-abort)
-  (set (make-local-variable 'po-subedit-message)
-       "Type 'C-s' once done, or 'C-g' to abort edit"))
 
 (defun ots-po-subedit-mode-set-properties ()
   "Set properties for editing PO file entries."
   (turn-off-auto-fill)
-  (visual-line-mode t))
+  (visual-line-mode 1)
+  (define-key po-subedit-mode-map (kbd "C-s") 'po-subedit-exit)
+  (define-key po-subedit-mode-map (kbd "C-g") 'po-subedit-abort)
+  (set (make-local-variable 'po-subedit-message)
+       "Type 'C-s' once done, or 'C-g' to abort edit"))
 
 (defadvice po-edit-string (around setup-spell-checking (string type expand-tabs) activate)
   "Set up spell-checking for the subedit buffer."
@@ -37,8 +35,7 @@
       (wcheck-change-language lang)
       (wcheck-mode))))
 
-(add-hook 'po-mode-hook 'ots-po-mode-set-keys)
-(add-hook 'po-subedit-mode-hook 'ots-po-subedit-mode-set-keys)
+(add-hook 'po-mode-hook 'ots-po-mode-set-properties)
 (add-hook 'po-subedit-mode-hook 'ots-po-subedit-mode-set-properties)
 (modify-coding-system-alist 'file "\\.po\\'" 'utf-8)
 
