4e5d4614d9510722b6b92b7ae91cf5b78b6d5856
Osmo Salomaa <osmo.salomaa@strafica.fi>
2017-01-05 15:24:12 +0200

    When saving, backup files by copying
    The default of renaming the file before saving seems to make the file
    disappear in Nautilus when saving the file on a samba share in Linux,
    requiring one hit Ctrl+R to see the file again.
    
    http://superuser.com/a/1073675
    http://www.gnu.org/software/emacs/manual/html_node/emacs/Backup-Copying.html

--- init-lisp/ots-backup.el
+++ init-lisp/ots-backup.el
@@ -5,11 +5,13 @@
   (concat temporary-file-directory "emacs/backup/")
   "Directory to save backup files in.")
 
-(make-directory ots-backup-directory t)
-(setq backup-directory-alist `((".*" . ,ots-backup-directory)))
 (setq auto-save-file-name-transforms `((".*" ,ots-backup-directory t)))
 (setq auto-save-list-file-prefix ots-backup-directory)
+(setq backup-by-copying t)
+(setq backup-directory-alist `((".*" . ,ots-backup-directory)))
+
 (add-hook 'change-major-mode-hook '(lambda () (setq create-lockfiles nil)))
+(make-directory ots-backup-directory t)
 
 (provide 'ots-backup)
 ;;; ots-backup.el ends here
