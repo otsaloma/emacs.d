d23ee79c4921af106d95c25fa6b4452b6e64761f
Osmo Salomaa <otsaloma@iki.fi>
2017-01-06 20:55:27 +0200

    Update start scripts for HiDPI screen

--- extra/emacs.lua
+++ extra/emacs.lua
@@ -2,6 +2,5 @@
 -- http://unix.stackexchange.com/q/14129
 if (string.lower(get_window_class()) == "emacs" and
     get_window_type() == "WINDOW_TYPE_NORMAL") then
-   os.execute("xprop -id " .. get_window_xid() ..
-      " -f _GTK_THEME_VARIANT 8u -set _GTK_THEME_VARIANT dark")
+   os.execute("xprop -id " .. get_window_xid() .. " -f _GTK_THEME_VARIANT 8u -set _GTK_THEME_VARIANT dark")
 end
