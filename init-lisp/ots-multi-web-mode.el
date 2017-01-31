c39a4649632ffb403b91f313a8d812c5ad8340c9
Osmo Salomaa <osmo.salomaa@strafica.fi>
2017-01-31 17:54:28 +0200

    Add config for HTML + CSS and JavaScript

--- init-lisp/ots-multi-web-mode.el
+++ init-lisp/ots-multi-web-mode.el
@@ -3,11 +3,22 @@
 
 (require 'multi-web-mode)
 
-;; HTML in Javascript/ES6 template literals.
+;; XXX: multi-web-mode doesn't seem to work with mode-specific hooks and/or
+;; buffer-local definitions, so we can only use one global config at a time.
+
+;; JavaScript + HTML in template literals
 (setq mweb-default-major-mode 'js-mode)
 (setq mweb-tags '((html-mode "`\n? *<" ">`")))
 (setq mweb-filename-extensions '("js"))
 (multi-web-global-mode 1)
+
+;; HTML + CSS and JavaScript
+;; (setq mweb-default-major-mode 'html-mode)
+;; (setq mweb-tags
+;;       '((css-mode "<style>" "</style>")
+;;         (js-mode "<script>" "</script>")))
+;; (setq mweb-filename-extensions '("html"))
+;; (multi-web-global-mode 1)
 
 (provide 'ots-multi-web-mode)
 ;;; ots-multi-web-mode.el ends here
