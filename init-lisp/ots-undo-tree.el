ea7d30a556302f487d745a386b27c8874c0448df
Osmo Salomaa <otsaloma@iki.fi>
2017-05-03 01:23:10 +0300

    Disable buggy and weird undo in region

--- init-lisp/ots-undo-tree.el
+++ init-lisp/ots-undo-tree.el
@@ -4,6 +4,7 @@
 (require 'undo-tree)
 (defalias 'redo 'undo-tree-redo)
 (defalias 'redo 'undo-tree-undo)
+(setq undo-tree-enable-undo-in-region nil)
 (global-undo-tree-mode 1)
 
 (provide 'ots-undo-tree)
