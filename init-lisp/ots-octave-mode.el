7cab53ced7ef5dc039ba905fde8823fdadb5b5b0
Osmo Salomaa <otsaloma@iki.fi>
2017-05-30 23:32:00 +0300

    Fixes for octave-mode

--- init-lisp/ots-octave-mode.el
+++ init-lisp/ots-octave-mode.el
@@ -3,8 +3,18 @@
 
 (add-to-list 'auto-mode-alist '("\\.m$" . octave-mode))
 
+(defun ots-octave-mode-set-indentation ()
+  "Fix stupid indentation of single character comments."
+  ;; https://emacs.stackexchange.com/a/15170
+  (defun octave-indent-comment ()
+    (save-excursion
+      (back-to-indentation)
+      (cond ((octave-in-string-or-comment-p) nil)
+            ((looking-at-p "\\(\\s<\\)\\1\\{2,\\}") 0)))))
+
 (defun ots-octave-mode-set-keys ()
   "Set keybindings for editing Octave files."
+  (local-set-key (kbd "<backspace>") 'backward-delete-char-untabify)
   (local-set-key (kbd "<f2>") 'helm-dash-at-point)
   (local-set-key (kbd "<f6>") 'run-octave)
   (local-set-key (kbd "<f8>") 'octave-send-buffer)
@@ -13,6 +23,7 @@
 (defun ots-octave-mode-set-properties ()
   "Set properties for editing Octave files."
   (eldoc-mode 1)
+  (setq comment-add 0)
   (setq comment-start "%")
   (setq-local company-backends
    '((company-keywords company-dabbrev-code)
@@ -28,6 +39,7 @@
   (setq-local helm-dash-docsets '("Octave"))
   (turn-on-auto-fill))
 
+(add-hook 'octave-mode-hook 'ots-octave-mode-set-indentation)
 (add-hook 'octave-mode-hook 'ots-octave-mode-set-keys)
 (add-hook 'octave-mode-hook 'ots-octave-mode-set-properties)
 
