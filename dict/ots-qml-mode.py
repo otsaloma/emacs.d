830b71a6de4d973358be229f18d5928ca8e3f712
Osmo Salomaa <otsaloma@iki.fi>
2017-06-22 01:46:33 +0300

    Better filtering for QML dict

--- dict/ots-qml-mode.py
+++ dict/ots-qml-mode.py
@@ -4,28 +4,25 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, os.path.join(".."))
 import docsets
 names = []
-root = "https://doc.qt.io/qt-5/"
-soup = docsets.soup_from_url("https://doc.qt.io/qt-5/qmltypes.html")
+root = "https://doc.qt.io/qt-5.6/"
+soup = docsets.soup_from_url("https://doc.qt.io/qt-5.6/qmltypes.html")
 for tag in soup.find_all("a", dict(href=re.compile("qml-.*html"))):
-    names.append(tag.text.strip())
+    names.append(tag.text.split(":")[0].strip())
     path = root + tag.attrs["href"].strip()
     soup = docsets.soup_from_url(path)
-    for tag in soup.find_all("a", dict(href=re.compile("-(prop|method)$"))):
-        names.append(tag.text.strip())
+    for li in soup.find_all("li", class_="fn"):
+        attrs = dict(href=re.compile("-(prop|method)$"))
+        for tag in li.find_all("a", attrs):
+            names.append(tag.text.strip())
 root = "https://sailfishos.org/develop/docs/silica/"
 soup = docsets.soup_from_url("https://sailfishos.org/develop/docs/silica/sailfish-silica-all.html")
 for tag in soup.find_all("a", dict(href=re.compile("qml-.*html"))):
-    names.append(tag.text.strip())
+    names.append(tag.text.split(":")[0].strip())
     path = root + tag.attrs["href"].strip()
     soup = docsets.soup_from_url(path)
-    for tag in soup.find_all("a", dict(href=re.compile("-(prop|method)$"))):
-        names.append(tag.text.strip())
-for i in range(len(names)):
-    names[i] = re.sub(r"^.+::", "", names[i])
-    names[i] = re.sub(r"\(.+$", "", names[i])
-    names[i] = re.sub(r": .+$", "", names[i])
-names = list(filter(lambda x: x[0].isalpha(), names))
-names = list(filter(lambda x: len(x) > 2, names))
-names = list(filter(lambda x: not " " in x, names))
+    for li in soup.find_all("li", class_="fn"):
+        attrs = dict(href=re.compile("-(prop|method)$"))
+        for tag in li.find_all("a", attrs):
+            names.append(tag.text.strip())
 names = sorted(set(names))
 open("ots-qml-mode", "w").write("\n".join(names) + "\n")
