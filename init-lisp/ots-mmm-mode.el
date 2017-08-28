39fc1fb01990d6cfd15f5114a4f7cd4b764a5e7e
Osmo Salomaa <otsaloma@iki.fi>
2017-08-29 01:17:50 +0300

    Use html-erb-mode

--- init-lisp/ots-mmm-mode.el
+++ init-lisp/ots-mmm-mode.el
@@ -4,41 +4,46 @@
 (require 'mmm-mode)
 (require 'mmm-auto)
 
+;; We need html-erb-mode to get submode indentation correct.
+(add-to-list 'auto-mode-alist '("\\.html\\'" . html-erb-mode))
+
 (setq mmm-global-mode 'maybe)
 (setq mmm-submode-decoration-level 2)
 
-;; CSS in HTML
+;; HTML: CSS <style> block
 
 (mmm-add-group
  'html-css
  '((css-block
     :submode css-mode
-    :front "<style>"
-    :back "</style>")))
+    :front "<style[^>]*>[ \t]*\n?"
+    :back "[ \t]*</style>")))
 
-(mmm-add-mode-ext-class 'html-mode nil 'html-css)
+(mmm-add-mode-ext-class 'html-erb-mode nil 'html-css)
 
-;; HTML in JavaScript
+;; HTML: JavaScript <script> block
+
+(mmm-add-group
+ 'html-js
+ '((js-block
+    :submode js-mode
+    :front "<script[^>]*>[ \t]*\n?"
+    :back "[ \t]*</script>")))
+
+(mmm-add-mode-ext-class 'html-erb-mode nil 'html-js)
+
+;; JavaScript: HTML in template literals
 
 (mmm-add-group
  'js-html
  '((html-block
     :submode html-mode
     :front "`[\n\t ]*<"
-    :back ">[\n\t ]*`")))
+    :front-offset -1
+    :back ">[\n\t ]*`"
+    :back-offset 1)))
 
 (mmm-add-mode-ext-class 'js-mode nil 'js-html)
-
-;; JavaScript in HTML
-
-(mmm-add-group
- 'html-js
- '((js-block
-    :submode js-mode
-    :front "<script>"
-    :back "</script>")))
-
-(mmm-add-mode-ext-class 'html-mode nil 'html-js)
 
 (provide 'ots-mmm-mode)
 ;;; ots-mmm-mode.el ends here
