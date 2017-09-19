ded5c7c25141d67f8885cdd4698d8c19d4683b25
Osmo Salomaa <osmo.salomaa@strafica.fi>
2017-09-19 19:25:55 +0300

    Drop the require

--- init-lisp/ots-json-mode.el
+++ init-lisp/ots-json-mode.el
@@ -1,16 +1,15 @@
 ;;; -*- coding: utf-8 -*-
 ;;; ots-json-mode.el
 
-(require 'json-mode)
-
 (defun ots-json-mode-set-properties ()
   "Set properties for editing JSON files."
   (setq fill-column 1000)
   (turn-off-auto-fill))
 
 (add-hook 'json-mode-hook 'ots-json-mode-set-properties)
+(add-to-list 'auto-mode-alist '("\\.jshintrc\\'" . json-mode))
+(add-to-list 'auto-mode-alist '("\\.tern-project\\'" . json-mode))
 (modify-coding-system-alist 'file "\\.json\\'" 'utf-8)
-(setq json-mode-auto-mode-list '(".jshintrc" ".tern-project"))
 
 (provide 'ots-json-mode)
 ;;; ots-json-mode.el ends here
