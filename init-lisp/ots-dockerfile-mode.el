572f13316a496360442a31fd955274deac468b9b
Osmo Salomaa <osmo@blok.ai>
2018-05-29 11:50:02 +0300

    Tweak Dockerfile filename pattern

--- init-lisp/ots-dockerfile-mode.el
+++ init-lisp/ots-dockerfile-mode.el
@@ -2,8 +2,8 @@
 ;;; ots-dockerfile-mode.el
 
 (autoload 'dockerfile-mode "dockerfile-mode" "Dockerfile" t)
-(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
-(modify-coding-system-alist 'file "Dockerfile\\'" 'utf-8)
+(add-to-list 'auto-mode-alist '("\\<Dockerfile" . dockerfile-mode))
+(modify-coding-system-alist 'file "\\<Dockerfile" 'utf-8)
 
 (provide 'ots-dockerfile-mode)
 ;;; ots-dockerfile-mode.el ends here
