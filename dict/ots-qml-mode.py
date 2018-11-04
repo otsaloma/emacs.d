ebb1c9f5eda0b1e99b22683a1ee465dc434a72b1
Osmo Salomaa <otsaloma@iki.fi>
2018-11-04 02:53:19 +0200

    Update completion dicts

--- dict/ots-qml-mode.py
+++ dict/ots-qml-mode.py
@@ -14,15 +14,5 @@ for tag in soup.find_all("a", dict(href=re.compile("qml-.*html"))):
         attrs = dict(href=re.compile("-(prop|method)$"))
         for tag in li.find_all("a", attrs):
             names.append(tag.text.strip())
-root = "https://sailfishos.org/develop/docs/silica/"
-soup = docsets.soup_from_url("https://sailfishos.org/develop/docs/silica/sailfish-silica-all.html")
-for tag in soup.find_all("a", dict(href=re.compile("qml-.*html"))):
-    names.append(tag.text.split(":")[0].strip())
-    path = root + tag.attrs["href"].strip()
-    soup = docsets.soup_from_url(path)
-    for li in soup.find_all("li", class_="fn"):
-        attrs = dict(href=re.compile("-(prop|method)$"))
-        for tag in li.find_all("a", attrs):
-            names.append(tag.text.strip())
 names = sorted(set(names))
 open("ots-qml-mode", "w").write("\n".join(names) + "\n")
