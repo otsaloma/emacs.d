44111b520759da371971caf55e3242288c983a07
Osmo Salomaa <otsaloma@iki.fi>
2018-11-24 20:00:56 +0200

    Update gpg config

--- init-lisp/ots-gpg.el
+++ init-lisp/ots-gpg.el
@@ -8,17 +8,34 @@
 
 (pinentry-start)
 
-(defun ots-gpg-mask ()
-  "Hide contents of all lines beginning with a space."
-  (when (string= (file-name-nondirectory buffer-file-name) "pass.gpg")
-    (font-lock-add-keywords
-     nil '(("^ \\(.*\\)$" 1 font-lock-keyword-face)))
-    (let ((default-bg (face-attribute 'default :background))
-          (default-fg (face-attribute 'default :foreground)))
-      (set-face-attribute 'region nil :foreground default-fg :strike-through nil)
-      (set-face-attribute 'font-lock-keyword-face nil :foreground default-bg :strike-through default-fg))))
+(defun ots-gpg-copy ()
+  "Copy phrase at point to the clipboard."
+  (interactive)
+  (kill-new
+   (replace-regexp-in-string "^.: *" ""
+    (replace-regexp-in-string "[ \n]*$" ""
+     (thing-at-point 'line t))))
+  (message "Copied to clipboard!"))
 
-(add-hook 'find-file-hook 'ots-gpg-mask)
+(defun ots-gpg-pass ()
+  "Add special font-lock for pass.gpg files."
+  (local-set-key (kbd "C-c C-c") 'ots-gpg-copy)
+  (local-set-key (kbd "<f5>") 'ots-gpg-copy)
+  (font-lock-add-keywords
+   nil '(("^#: +\\(.+\\)$" 1 font-lock-function-name-face)
+         ("^u: +\\(.+\\)$" 1 font-lock-variable-name-face)
+         ("^p: +\\(.+\\)$" 1 font-lock-keyword-face)))
+  (let ((default-fg (face-attribute 'default :foreground)))
+    (set-face-attribute 'font-lock-keyword-face nil
+                        :background default-fg
+                        :foreground default-fg)))
+
+(defun ots-gpg-pass-maybe ()
+  "Add special font-lock for pass.gpg files."
+  (when (string= (file-name-nondirectory buffer-file-name) "pass.gpg")
+    (ots-gpg-pass)))
+
+(add-hook 'find-file-hook 'ots-gpg-pass-maybe)
 
 (provide 'ots-gpg)
 ;;; ots-gpg.el ends here
