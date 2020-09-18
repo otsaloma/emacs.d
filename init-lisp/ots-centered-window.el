dff3402e18131b939f82f8afab983cbde974f12c
Osmo Salomaa <osmo@blok.ai>
2020-09-18 16:17:34 +0300

    Tweak neotree

--- init-lisp/ots-centered-window.el
+++ init-lisp/ots-centered-window.el
@@ -1,8 +1,8 @@
 ;;; -*- coding: utf-8 -*-
 ;;; ots-centered-window.el
 
-(require 'centered-window)
-(centered-window-mode t)
+;; (require 'centered-window)
+;; (centered-window-mode t)
 
 (provide 'ots-centered-window)
 ;;; ots-centered-window.el ends here
