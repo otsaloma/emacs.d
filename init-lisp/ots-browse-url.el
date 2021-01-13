8a04f503551b1a0b88efa056b4c54d8fe5b97e84
Osmo Salomaa <otsaloma@iki.fi>
2021-01-13 22:54:20 +0200

    Use xdg-open

--- init-lisp/ots-browse-url.el
+++ init-lisp/ots-browse-url.el
@@ -1,9 +1,7 @@
 ;;; -*- coding: utf-8 -*-
 ;;; ots-browse-url.el
 
-(setq browse-url-browser-function 'browse-url-firefox)
-(setq browse-url-firefox-new-window-is-tab t)
-(setq browse-url-new-window-flag t)
+(setq browse-url-browser-function 'browse-url-xdg-open)
 
 (provide 'ots-browse-url)
 ;;; ots-browse-url.el ends here
