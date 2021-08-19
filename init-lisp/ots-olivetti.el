91fb37466f399d046704cfd8d834d6fb1cddc91a
Osmo Salomaa <otsaloma@iki.fi>
2021-08-19 23:50:55 +0300

    Add olivetti

---
+++ init-lisp/ots-olivetti.el
@@ -0,0 +1,9 @@
+;;; -*- coding: utf-8 -*-
+;;; ots-olivetti.el
+
+(setq olivetti-body-width 120)
+
+(global-set-key (kbd "<C-f11>") 'olivetti-mode)
+
+(provide 'ots-olivetti)
+;;; ots-olivetti.el ends here
