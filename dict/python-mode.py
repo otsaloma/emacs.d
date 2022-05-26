ab4146b1ca56197ff6fd7a27f330cb662a39810c
Osmo Salomaa <otsaloma@iki.fi>
2022-05-27 01:52:54 +0300

    Add missing builtins

--- dict/python-mode.py
+++ dict/python-mode.py
@@ -4,9 +4,18 @@ import itertools
 
 from pathlib import Path
 
-clss = [bool, dict, float, int, list, set, str, tuple]
-objs = [x() for x in clss]
-names = itertools.chain.from_iterable(map(dir, objs))
+objects = [
+    __builtins__,
+    bool(),
+    dict(),
+    float(),
+    int(),
+    list(),
+    set(),
+    str(),
+    tuple(),
+]
+names = itertools.chain.from_iterable(map(dir, objects))
 names = sorted(set(filter(lambda x: x[0].isalpha(), names)))
 text = "\n".join(names) + "\n"
 Path("python-mode").write_text(text, "utf-8")
