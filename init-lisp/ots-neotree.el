970de29d185dd7138781f7c2ae6315dd9b221146
Osmo Salomaa <osmo@blok.ai>
2023-01-19 16:37:03 +0200

    Fix neotree background color

--- init-lisp/ots-neotree.el
+++ init-lisp/ots-neotree.el
@@ -49,7 +49,7 @@
           (neotree-dir (projectile-project-root)))
         (neotree-find file-name)
         (if (string= ots-theme-current "dark")
-            (setq buffer-face-mode-face `(:background "#252525"))
+            (setq buffer-face-mode-face `(:background "#242424"))
           (setq buffer-face-mode-face `(:background "#eeeeee")))
         (buffer-face-mode 1)
         (switch-to-buffer buffer)))))
