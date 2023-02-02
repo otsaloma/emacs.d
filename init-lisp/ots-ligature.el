85b7fe712064524d565e3a2e82d50a9210bec2a3
Osmo Salomaa <osmo@blok.ai>
2023-02-02 14:58:52 +0200

    Disable ligature-mode for ligature config

--- init-lisp/ots-ligature.el
+++ init-lisp/ots-ligature.el
@@ -1,13 +1,17 @@
 ;;; -*- coding: utf-8 -*-
 ;;; ots-ligature.el
 
+;; Local Variables:
+;; eval: (ligature-mode -1)
+;; End:
+
 (require 'ligature)
 
 ;; https://github.com/mickeynp/ligature.el
 ;; https://github.com/microsoft/cascadia-code#font-features
 (ligature-set-ligatures
  '(markdown-mode prog-mode text-mode)
- '("<-" "<--" "->" "-->" "<!--" "<==" "==>" "|>"))
+ '("<-" "<--" "<!--" "<==" "->" "-->" "==>" "|>"))
 
 (global-ligature-mode t)
 
