c1026906fabb21f2f64b3c5a913887c0b0cbf34e
Osmo Salomaa <osmo@blok.ai>
2023-06-30 17:06:16 +0300

    Make a distinction between different kinds of text-mode files

--- init-lisp/ots-text-mode.el
+++ init-lisp/ots-text-mode.el
@@ -1,28 +1,31 @@
 ;;; -*- coding: utf-8 -*-
 ;;; ots-text-mode.el
 
+;; Make a distinction between actual text files (*.txt) and various
+;; programming config files (any other extension) that either don't have
+;; an actual mode or have a mode that inherits from text-mode. Defer
+;; programming files to the corresponding functions in ots-prog-mode.
+
+(require 'olivetti)
+(require 'ots-prog-mode)
+
 (defun ots-text-mode-set-keys ()
   "Set keybindings for editing text files."
-  (local-set-key (kbd "<f2>") 'helm-dash-at-point)
-  (local-set-key (kbd "<backspace>") 'backward-delete-char-untabify))
+  (if (string-match-p "\\.txt$" (buffer-file-name))
+      (local-set-key (kbd "<backspace>") 'backward-delete-char-untabify)
+    (ots-prog-mode-set-keys)))
 
 (defun ots-text-mode-set-properties ()
   "Set properties for editing text files."
-  (unwind-protect (ignore-errors (eldoc-mode 1)) (message ""))
-  (unwind-protect (ignore-errors (hs-minor-mode 1)) (message ""))
-  (modify-syntax-entry ?_ "w")
-  (setq-local company-backends '((company-capf
-                                  company-keywords
-                                  company-dict
-                                  company-dabbrev-code
-                                  company-dabbrev)))
-
-  (setq-local comment-auto-fill-only-comments t)
-  (setq-local fill-column 72)
-  (setq-local indent-tabs-mode nil)
-  (setq-local tab-width 4)
-  (setq-local truncate-lines t)
-  (turn-on-auto-fill))
+  (if (string-match-p "\\.txt$" (buffer-file-name))
+      (progn (setq-local company-backends '((company-capf company-dabbrev)))
+             (setq-local fill-column 72)
+             (setq-local indent-tabs-mode nil)
+             (setq-local tab-width 4)
+             (setq-local truncate-lines nil)
+             (turn-off-auto-fill)
+             (olivetti-mode 1))
+    (ots-prog-mode-set-properties)))
 
 (add-hook 'text-mode-hook 'ots-text-mode-set-keys)
 (add-hook 'text-mode-hook 'ots-text-mode-set-properties)
