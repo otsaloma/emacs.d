a57057c08799c4dd422ee60e323bfc5f362d144b
Osmo Salomaa <otsaloma@iki.fi>
2023-12-30 01:47:09 +0200

    Set font in elisp
    No more .Xresources on Wayland.

--- init-lisp/ots-theme.el
+++ init-lisp/ots-theme.el
@@ -6,6 +6,8 @@
 
 (use-package emacs
   :config
+  (add-to-list 'default-frame-alist '(font . "Berkeley Mono-11"))
+  (setq-default line-spacing 0.4)
   (setq font-lock-maximum-decoration t)
   (when (file-exists-p "~/.emacs.d/theme-light")
     (require 'ots-theme-light)
@@ -19,16 +21,7 @@
   ;; Disable italics as most monospace fonts don't really have proper italics.
   ;; (set-face-attribute 'italic nil :slant 'normal)
   ;; (set-face-attribute 'bold-italic nil :slant 'normal)
-  ;; Set line spacing depending on font used.
-  (let ((family (face-attribute 'default :family)))
-    (setq-default line-spacing
-                  (cond ((string= family "Berkeley Mono")    0.40)
-                        ((string= family "Cascadia Code")    0.40)
-                        ((string= family "Cascadia Code PL") 0.40)
-                        ((string= family "Cascadia Mono")    0.40)
-                        ((string= family "Cascadia Mono PL") 0.40)
-                        ((string= family "IBM Plex Mono")    0.30)
-                        ((string= family "SF Mono")          0.50)))))
+  )
 
 (provide 'ots-theme)
 ;;; ots-theme.el ends here
