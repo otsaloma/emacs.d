ec51fb3348d8bc305c1be74488873b6be0ba8ea2
Osmo Salomaa <otsaloma@iki.fi>
2025-03-02 22:04:44 +0200

    Remove LaTeX faces config
    These seem to have changed.

--- init-lisp/ots-latex.el
+++ init-lisp/ots-latex.el
@@ -1,35 +1,6 @@
 ;;; -*- coding: utf-8 -*-
 ;;; ots-latex.el
 
-(defun ots-latex-set-face-sectioning (level)
-  "Set fonts and colors for section titles."
-  (let ((face (format "font-latex-sectioning-%d-face" level)))
-    (setq face (car (read-from-string face)))
-    (set-face-attribute face nil :background nil)
-    (set-face-attribute face nil :foreground nil)
-    (set-face-attribute face nil :inherit 'font-lock-negation-char-face)))
-
-(defun ots-latex-set-faces ()
-  "Set fonts and colors for editing LaTeX files."
-  (font-latex-add-keywords '(("frametitle" "[{")) 'slide-title)
-  (ots-latex-set-faces-sectioning)
-  (ots-latex-set-faces-verbatim))
-
-(defun ots-latex-set-faces-sectioning ()
-  "Set fonts and colors for section titles."
-  (dotimes (i 6) (ots-latex-set-face-sectioning i))
-  (let ((face 'font-latex-slide-title-face))
-    (set-face-attribute face nil :background nil)
-    (set-face-attribute face nil :foreground nil)
-    (set-face-attribute face nil :inherit 'font-lock-negation-char-face)))
-
-(defun ots-latex-set-faces-verbatim ()
-  "Set fonts and colors for verbatim text."
-  (make-local-variable 'LaTeX-verbatim-regexp)
-  (setq LaTeX-verbatim-regexp (concat LaTeX-verbatim-regexp "\\|Verbatim"))
-  (add-to-list 'LaTeX-verbatim-environments-local "Verbatim")
-  (font-latex-set-syntactic-keywords))
-
 (defun ots-latex-set-properties ()
   "Set properties for editing LaTeX files."
   (company-auctex-init)
@@ -46,7 +17,6 @@
 (use-package latex
   :mode ("\\.tex\\'" . latex-mode)
   :config
-  (add-hook 'LaTeX-mode-hook 'ots-latex-set-faces)
   (add-hook 'LaTeX-mode-hook 'ots-latex-set-properties))
 
 (provide 'ots-latex)
